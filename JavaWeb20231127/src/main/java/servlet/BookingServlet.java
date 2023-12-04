cb8d672f6b9b3a625b02a4d07dd50f92d2997792
vincenttuan <vincentjava@yahoo.com.tw>
2023-12-04 19:23:27 +0800

    java

--- JavaWeb20231127/src/main/java/servlet/BookingServlet.java
+++ JavaWeb20231127/src/main/java/servlet/BookingServlet.java
@@ -14,6 +14,10 @@ public class BookingServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
+		resp.setCharacterEncoding("utf-8");
+		resp.setContentType("text/plain;charset=utf-8");
+		
 		String contextPath = getServletContext().getContextPath();
 		String servletPath = req.getServletPath();
 		String pathInfo = req.getPathInfo();
@@ -22,6 +26,24 @@ public class BookingServlet extends HttpServlet {
 		out.println("contextPath = " + contextPath);
 		out.println("servletPath = " + servletPath);
 		out.println("pathInfo = " + pathInfo);
+		
+		switch(pathInfo) {
+			case "/bookRoom": // 預約房間
+				
+				break;
+			case "/cancelBooking": // 取消預約
+				
+				break;	
+			case "/viewBookings": // 查看預約
+				
+				break;
+			default: // 其他
+				resp.sendError(500, "別鬧了~");
+				break;
+		}
+		
 	}
+	
+	
 	
 }
