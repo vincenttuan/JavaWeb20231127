8ddb2ac9cfb0e24478f297e278a6a0a17f3a5b0e
vincenttuan <vincentjava@yahoo.com.tw>
2023-12-04 19:39:20 +0800

    java

--- JavaWeb20231127/src/main/java/servlet/BookingServlet.java
+++ JavaWeb20231127/src/main/java/servlet/BookingServlet.java
@@ -2,6 +2,11 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,7 +16,10 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(value = "/booking/*")
 public class BookingServlet extends HttpServlet {
-
+	
+	private static List<Map<String, Object>> bookings = new CopyOnWriteArrayList<>();
+	private AtomicInteger bookingIdCount = new AtomicInteger(0);
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setCharacterEncoding("utf-8");
@@ -29,6 +37,21 @@ public class BookingServlet extends HttpServlet {
 		
 		switch(pathInfo) {
 			case "/bookRoom": // 預約房間
+				
+				String roomId = req.getParameter("roomId");
+				String name = req.getParameter("name");
+				String date = req.getParameter("date");
+				
+				Map<String, Object> bookRoom = new LinkedHashMap<>();
+				int bookingId = bookingIdCount.incrementAndGet();
+				bookRoom.put("bookingId", bookingId);
+				bookRoom.put("roomId", roomId);
+				bookRoom.put("name", name);
+				bookRoom.put("date", date);
+				
+				bookings.add(bookRoom);
+				
+				out.print("預約成功, 預約編號: " + bookingId);
 				
 				break;
 			case "/cancelBooking": // 取消預約
