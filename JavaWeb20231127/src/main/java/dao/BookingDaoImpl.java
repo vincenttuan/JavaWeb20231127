9233e57d55438378c87e7d8f8749adba50de0f85
vincenttuan <vincentjava@yahoo.com.tw>
2023-12-08 21:03:23 +0800

    java

---
+++ JavaWeb20231127/src/main/java/dao/BookingDaoImpl.java
@@ -0,0 +1,46 @@
+package dao;
+
+import java.util.List;
+
+import dao.model.BookingRoom;
+import dao.model.Room;
+
+public class BookingDaoImpl implements BookingDao {
+	
+	@Override
+	public int addBookingRoom(BookingRoom bookingRoom) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int cancelBookingRoomById(Integer bookingId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<BookingRoom> findAllBookingRooms() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BookingRoom getBookingRoomById(Integer bookingId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Room> findAllRooms() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Room getRoomById(Integer roomId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
