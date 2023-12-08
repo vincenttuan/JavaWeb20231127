06b28c83bcd90e1dc995b09ccc1ba1178de779d0
vincenttuan <vincentjava@yahoo.com.tw>
2023-12-08 20:34:17 +0800

    java

--- JavaWeb20231127/src/main/java/dao/model/Room.java
+++ JavaWeb20231127/src/main/java/dao/model/Room.java
@@ -1,5 +1,28 @@
 package dao.model;
 
+// Entity 物件
 public class Room {
-
+	private Integer roomId;
+	private String name;
+	
+	public Integer getRoomId() {
+		return roomId;
+	}
+	public void setRoomId(Integer roomId) {
+		this.roomId = roomId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Room [roomId=" + roomId + ", name=" + name + "]";
+	}
+	
+	
+	
 }
