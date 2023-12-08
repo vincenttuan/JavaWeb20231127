12035ece164b6cb845835bee810d945e92736513
vincenttuan <vincentjava@yahoo.com.tw>
2023-12-08 20:48:58 +0800

    java

--- JavaWeb20231127/src/main/java/dao/BookingDao.java
+++ JavaWeb20231127/src/main/java/dao/BookingDao.java
@@ -1,5 +1,15 @@
 package dao;
 
-public class BookingDao {
+import java.util.List;
 
+import dao.model.BookingRoom;
+import dao.model.Room;
+
+public interface BookingDao {
+	int addBookingRoom(BookingRoom bookingRoom);
+	int cancelBookingRoomById(Integer bookingId);
+	List<BookingRoom> findAllBookingRooms();
+	BookingRoom getBookingRoomById(Integer bookingId);
+	List<Room> findAllRooms();
+	Room getRoomById(Integer roomId);
 }
