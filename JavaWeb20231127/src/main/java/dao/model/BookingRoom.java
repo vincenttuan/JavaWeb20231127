8fde7117b706d579ad7c24aad8ac4e0f71b5bf58
vincenttuan <vincentjava@yahoo.com.tw>
2023-12-08 20:39:21 +0800

    java

--- JavaWeb20231127/src/main/java/dao/model/BookingRoom.java
+++ JavaWeb20231127/src/main/java/dao/model/BookingRoom.java
@@ -10,41 +10,63 @@ public class BookingRoom {
 	private String date;
 	private Timestamp createTime;
 	
+	// 關聯欄位
+	private Room room;
+
 	public Integer getBookingId() {
 		return bookingId;
 	}
+
 	public void setBookingId(Integer bookingId) {
 		this.bookingId = bookingId;
 	}
+
 	public Integer getRoomId() {
 		return roomId;
 	}
+
 	public void setRoomId(Integer roomId) {
 		this.roomId = roomId;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getDate() {
 		return date;
 	}
+
 	public void setDate(String date) {
 		this.date = date;
 	}
+
 	public Timestamp getCreateTime() {
 		return createTime;
 	}
+
 	public void setCreateTime(Timestamp createTime) {
 		this.createTime = createTime;
 	}
+
+	public Room getRoom() {
+		return room;
+	}
+
+	public void setRoom(Room room) {
+		this.room = room;
+	}
+
 	@Override
 	public String toString() {
 		return "BookingRoom [bookingId=" + bookingId + ", roomId=" + roomId + ", name=" + name + ", date=" + date
-				+ ", createTime=" + createTime + "]";
+				+ ", createTime=" + createTime + ", room=" + room + "]";
 	}
+	
 	
 	
 }
