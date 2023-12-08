33a88764464066bd39f9c73dd0dd4e8851bd73aa
vincenttuan <vincentjava@yahoo.com.tw>
2023-12-08 20:36:59 +0800

    java

--- JavaWeb20231127/src/main/java/dao/model/BookingRoom.java
+++ JavaWeb20231127/src/main/java/dao/model/BookingRoom.java
@@ -1,5 +1,50 @@
 package dao.model;
 
-public class BookingRoom {
+import java.sql.Timestamp;
 
+// Entity 物件
+public class BookingRoom {
+	private Integer bookingId;
+	private Integer roomId;
+	private String name;
+	private String date;
+	private Timestamp createTime;
+	
+	public Integer getBookingId() {
+		return bookingId;
+	}
+	public void setBookingId(Integer bookingId) {
+		this.bookingId = bookingId;
+	}
+	public Integer getRoomId() {
+		return roomId;
+	}
+	public void setRoomId(Integer roomId) {
+		this.roomId = roomId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public Timestamp getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Timestamp createTime) {
+		this.createTime = createTime;
+	}
+	@Override
+	public String toString() {
+		return "BookingRoom [bookingId=" + bookingId + ", roomId=" + roomId + ", name=" + name + ", date=" + date
+				+ ", createTime=" + createTime + "]";
+	}
+	
+	
 }
