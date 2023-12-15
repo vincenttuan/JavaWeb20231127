b14bd7e250bb28c9be733db6105a7128065007bb
vincenttuan <vincentjava@yahoo.com.tw>
2023-12-15 20:59:56 +0800

    java

--- JavaWeb20231127/src/main/java/servlet/UploadProductServlet.java
+++ JavaWeb20231127/src/main/java/servlet/UploadProductServlet.java
@@ -1,8 +1,26 @@
 package servlet;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/upload/product")
-public class UploadProductServlet {
+@MultipartConfig(
+		location = "C:\\FileServer\\upload\\product",
+		fileSizeThreshold = 1024 * 1024 * 2, // 2MB 會暫存在記憶體處理, 反之則會暫存在虛擬記憶體預處理
+		maxFileSize = 1024 * 1024 * 10 // 檔案限制最大 size
+)
+public class UploadProductServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String name = req.getParameter("productName");
+		resp.getWriter().println("name: " + name);
+	}
 	
 }
