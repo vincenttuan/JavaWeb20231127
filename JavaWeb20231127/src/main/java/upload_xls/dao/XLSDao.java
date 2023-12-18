12a52ab7577ac382c726b8df474b099981e49b72
vincenttuan <vincentjava@yahoo.com.tw>
2023-12-18 21:05:14 +0800

    java

--- JavaWeb20231127/src/main/java/upload_xls/dao/XLSDao.java
+++ JavaWeb20231127/src/main/java/upload_xls/dao/XLSDao.java
@@ -1,5 +1,33 @@
 package upload_xls.dao;
 
-public class XLSDao {
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import dao.model.BookingRoom;
+
+public class XLSDao {
+	
+private JdbcTemplate jdbcTemplate;
+	
+	public XLSDao() {
+		String driverName = "com.mysql.cj.jdbc.Driver";
+		String dbURL = "jdbc:mysql://localhost:3306/web?serverTimezone=Asia/Taipei";
+		String username = "root";
+		String password = "12345678";
+		
+		// 設定資料來源
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(driverName);
+		dataSource.setUrl(dbURL);
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
+		
+		// 將資料來源注入給 jdbcTemplate
+		jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
+	public int addEmployee(String name, String sex, int salary, int age) {
+		
+	}
+	
 }
