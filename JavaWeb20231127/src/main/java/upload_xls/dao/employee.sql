94f9523f1af6339e36cdb4a483005554e66ea5d1
vincenttuan <vincentjava@yahoo.com.tw>
2023-12-18 20:57:12 +0800

    java

---
+++ JavaWeb20231127/src/main/java/upload_xls/dao/employee.sql
@@ -0,0 +1,10 @@
+-- name sex salary age
+-- John M 50000 30
+create table if not exists employee(
+	id int auto_increment primary key,
+    name varchar(50) unique,
+    sex varchar(2),
+    salary int,
+    age int,
+    createtime timestamp default current_timestamp
+);
