01f0b64fb4b533a98006df2286ae313e3d3de1d1
vincenttuan <vincentjava@yahoo.com.tw>
2023-12-18 21:38:21 +0800

    java

--- JavaWeb20231127/src/main/java/upload_xls/dao/XLSDao.java
+++ JavaWeb20231127/src/main/java/upload_xls/dao/XLSDao.java
@@ -1,9 +1,15 @@
 package upload_xls.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import dao.model.BookingRoom;
+import upload_xls.entity.Employee;
 
 public class XLSDao {
 	
@@ -30,5 +36,29 @@ private JdbcTemplate jdbcTemplate;
 		String sql = "insert into employee(name, sex, salary, age) values(?, ?, ?, ?)";
 		return jdbcTemplate.update(sql, name, sex, salary, age);
 	}
+	
+	public int[] batchAddemployees(List<Employee> employees) {
+		String sql = "insert into employee(name, sex, salary, age) values(?, ?, ?, ?)";
+		
+		BatchPreparedStatementSetter setter = new BatchPreparedStatementSetter() {
+			
+			@Override
+			public void setValues(PreparedStatement ps, int i) throws SQLException {
+				Employee employee = employees.get(i);
+				ps.setString(1, employee.getName());
+				ps.setString(2, employee.getSex());
+				ps.setInt(3, employee.getSalary());
+				ps.setInt(4, employee.getAge());
+			}
+			
+			@Override
+			public int getBatchSize() {
+				return employees.size();
+			}
+		};
+		
+		int[] counts = jdbcTemplate.batchUpdate(sql, setter); 
+		return counts;		
+	} 
 	
 }
