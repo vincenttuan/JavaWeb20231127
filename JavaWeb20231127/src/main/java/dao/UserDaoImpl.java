1ca2afd6be8af1762239035df0a7099480b2c7ca
vincenttuan <vincentjava@yahoo.com.tw>
2023-12-22 20:34:37 +0800

    java

---
+++ JavaWeb20231127/src/main/java/dao/UserDaoImpl.java
@@ -0,0 +1,44 @@
+package dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import dao.model.User;
+
+public class UserDaoImpl implements UserDao {
+	
+	private JdbcTemplate jdbcTemplate;
+	
+	public UserDaoImpl() {
+		String driverName = "com.mysql.cj.jdbc.Driver";
+		String dbURL = "jdbc:mysql://localhost:3306/web?serverTimezone=Asia/Taipei";
+		String username = "root";
+		String password = "12345678";
+		
+		// 設定資料來源
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(driverName);
+		dataSource.setUrl(dbURL);
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
+		
+		// 將資料來源注入給 jdbcTemplate
+		jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
+	@Override
+	public User login(String username, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Map<String, Object>> findServicesByActorId(Integer a_Id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
