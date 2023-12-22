463d14fb8e47c598cef9e37a5b7c452affa37f93
vincenttuan <vincentjava@yahoo.com.tw>
2023-12-22 21:12:46 +0800

    java

--- JavaWeb20231127/src/main/java/servlet/UserLoginController.java
+++ JavaWeb20231127/src/main/java/servlet/UserLoginController.java
@@ -2,6 +2,7 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,7 +15,8 @@ public class UserLoginController extends HttpServlet {
 	// 登入畫面
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/views/userLogin.jsp");
+		rd.forward(req, resp);
 	}
 	
 	// 驗證登入
