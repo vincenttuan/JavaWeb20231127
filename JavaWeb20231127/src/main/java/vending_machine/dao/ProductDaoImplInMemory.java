8e86ce9819be01a639f814137d7c0c346acf6d5f
vincenttuan <vincentjava@yahoo.com.tw>
2023-12-25 21:10:19 +0800

    java

--- JavaWeb20231127/src/main/java/vending_machine/dao/ProductDaoImplInMemory.java
+++ JavaWeb20231127/src/main/java/vending_machine/dao/ProductDaoImplInMemory.java
@@ -10,9 +10,9 @@ public class ProductDaoImplInMemory implements ProductDao {
 	
 	public ProductDaoImplInMemory() {
 		products = new ArrayList<>();
-		products.add(new Product(1, "台灣啤酒", "beer", 50, 10));
+		products.add(new Product(1, "台灣啤酒", "beer", 50, 5));
 		products.add(new Product(2, "可口可樂", "cola", 25, 10));
-		products.add(new Product(3, "柚子茶", "crapefruit", 20, 10));
+		products.add(new Product(3, "柚子茶", "crapefruit", 20, 7));
 		products.add(new Product(4, "提神飲料", "energy", 30, 10));
 		products.add(new Product(5, "檸檬茶", "lemon", 20, 10));
 		products.add(new Product(6, "橘子汽水", "orange", 35, 10));
