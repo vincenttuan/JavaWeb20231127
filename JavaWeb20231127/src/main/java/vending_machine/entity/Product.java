51016fe24e3eee4f8971ba2528da41ed0a9c65df
vincenttuan <vincentjava@yahoo.com.tw>
2023-12-25 20:41:20 +0800

    java

---
+++ JavaWeb20231127/src/main/java/vending_machine/entity/Product.java
@@ -0,0 +1,42 @@
+package vending_machine.entity;
+
+public class Product {
+	private Integer id;
+	private String name;
+	private String imageName;
+	private Integer price;
+	private Integer stock;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getImageName() {
+		return imageName;
+	}
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	public Integer getStock() {
+		return stock;
+	}
+	public void setStock(Integer stock) {
+		this.stock = stock;
+	}
+	
+	
+}
