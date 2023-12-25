69ef2532a13a0b4fbb190f3a815ef2aac77ba823
vincenttuan <vincentjava@yahoo.com.tw>
2023-12-25 20:57:53 +0800

    java

--- JavaWeb20231127/src/main/java/vending_machine/MainServlet.java
+++ JavaWeb20231127/src/main/java/vending_machine/MainServlet.java
@@ -9,12 +9,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import vending_machine.dao.ProductDao;
+import vending_machine.dao.ProductDaoImplInMemory;
+
 @WebServlet("/vending_machine/main")
 public class MainServlet extends HttpServlet {
-
+	
+	private ProductDao productDao = new ProductDaoImplInMemory();
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/views/vending_machine/main.jsp");
+		req.setAttribute("products", productDao.findAllProducts());
 		rd.forward(req, resp);
 	}
 	
