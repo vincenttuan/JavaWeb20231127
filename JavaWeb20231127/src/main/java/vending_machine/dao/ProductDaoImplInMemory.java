ba450dc9aec7f1dbbe1614d808212b858dc45105
vincenttuan <vincentjava@yahoo.com.tw>
2023-12-25 20:44:55 +0800

    java

---
+++ JavaWeb20231127/src/main/java/vending_machine/dao/ProductDaoImplInMemory.java
@@ -0,0 +1,27 @@
+package vending_machine.dao;
+
+import java.util.List;
+
+import vending_machine.entity.Product;
+
+public class ProductDaoImplInMemory implements ProductDao {
+	
+	@Override
+	public List<Product> findAllProducts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product getProductByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product getProductById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
