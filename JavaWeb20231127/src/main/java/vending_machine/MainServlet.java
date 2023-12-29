528a23b9fce022023eed5223d353ffa062d7a358
vincenttuan <vincentjava@yahoo.com.tw>
2023-12-29 21:24:44 +0800

    java

--- JavaWeb20231127/src/main/java/vending_machine/MainServlet.java
+++ JavaWeb20231127/src/main/java/vending_machine/MainServlet.java
@@ -27,12 +27,13 @@ public class MainServlet extends HttpServlet {
 		List<Product> products = productDao.findAllProducts(); // 所有的進貨資料
 		List<SalesItem> salesItems = productDao.findAllSalesItems(); // 所有的銷貨資料
 		
+		// deepClone
 		List<Product> cloneProducts = new ArrayList<>();
 		products.forEach(product -> {
 			cloneProducts.add(new Product(product.getId(), product.getName(), product.getImageName(), product.getPrice(), product.getStock()));
 		});
 		
-		req.setAttribute("products", productDao.findAllProducts());
+		req.setAttribute("products", cloneProducts);
 		rd.forward(req, resp);
 	}
 	
