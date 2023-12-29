032292dfc33c9d0709b94e01806c8e4b39a99b1e
vincenttuan <vincentjava@yahoo.com.tw>
2023-12-29 20:42:14 +0800

    java

---
+++ JavaWeb20231127/src/main/java/vending_machine/servlet/SubmitServlet.java
@@ -0,0 +1,23 @@
+package vending_machine.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(value = "/vending/submit")
+public class SubmitServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html;charset=UTF-8");
+		
+		resp.getWriter().print(req.getParameterMap());
+	}
+	
+}
