feb8bb473f63baac77b28792a3029fe9f1103bfd
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-05 19:03:11 +0800

    java

--- JavaWeb20231127/src/main/java/vending_machine/entity/SalesItem.java
+++ JavaWeb20231127/src/main/java/vending_machine/entity/SalesItem.java
@@ -2,20 +2,23 @@ package vending_machine.entity;
 
 // 銷貨紀錄
 public class SalesItem {
-	private Integer productId;
+	private Integer productId; // 商品 id
 	private String productName;
 	private Integer totalPrice; // 銷售金額
 	private Integer totalAmount; // 銷售數量
+	// 該銷售紀錄是屬於哪一個使用者 
+	private Integer userId; // 使用者 id
 	
 	public SalesItem() {
 		
 	}
 	
-	public SalesItem(Integer productId, String productName, Integer totalPrice, Integer totalAmount) {
+	public SalesItem(Integer productId, String productName, Integer totalPrice, Integer totalAmount, Integer userId) {
 		this.productId = productId;
 		this.productName = productName;
 		this.totalPrice = totalPrice;
 		this.totalAmount = totalAmount;
+		this.userId = userId;
 	}
 
 	public Integer getProductId() {
@@ -43,11 +46,20 @@ public class SalesItem {
 		this.totalAmount = totalAmount;
 	}
 
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
 	@Override
 	public String toString() {
 		return "SalesItem [productId=" + productId + ", productName=" + productName + ", totalPrice=" + totalPrice
-				+ ", totalAmount=" + totalAmount + "]";
+				+ ", totalAmount=" + totalAmount + ", userId=" + userId + "]";
 	}
+	
 	
 	
 }
