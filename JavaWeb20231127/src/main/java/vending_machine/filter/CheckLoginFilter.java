5a2ee393f62123f3d23679dc859cb3b243d0c9ac
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-05 19:28:15 +0800

    java

--- JavaWeb20231127/src/main/java/vending_machine/filter/CheckLoginFilter.java
+++ JavaWeb20231127/src/main/java/vending_machine/filter/CheckLoginFilter.java
@@ -1,8 +1,23 @@
 package vending_machine.filter;
 
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-@WebFilter(value = {"/vending/main", "/vending/submit"})
-public class CheckLoginFilter {
+//@WebFilter(value = {"/vending/main", "/vending/submit"})
+//@WebFilter(value = {"/vending/*"})
+@WebFilter("/vending/*")
+public class CheckLoginFilter extends HttpFilter {
 
+	@Override
+	protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		
+	}
+	
 }
