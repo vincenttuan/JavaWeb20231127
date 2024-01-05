fe4bc091730ddae1af0fafe74f3b8155342e0d63
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-05 20:44:05 +0800

    java

--- JavaWeb20231127/src/main/java/vending_machine/servlet/LogoutServlet.java
+++ JavaWeb20231127/src/main/java/vending_machine/servlet/LogoutServlet.java
@@ -16,7 +16,6 @@ public class LogoutServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		HttpSession session = req.getSession();
-		session.setAttribute("user", null);
 		session.invalidate(); // session 失效
 		
 		// 指向 /vending/main 首頁
