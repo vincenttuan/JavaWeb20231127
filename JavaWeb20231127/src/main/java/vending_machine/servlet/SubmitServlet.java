a4d3ba34e4b50cc9a05e1b4760881f4f29bd7185
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-05 20:59:04 +0800

    java

--- JavaWeb20231127/src/main/java/vending_machine/servlet/SubmitServlet.java
+++ JavaWeb20231127/src/main/java/vending_machine/servlet/SubmitServlet.java
@@ -1,10 +1,12 @@
 package vending_machine.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -33,6 +35,8 @@ public class SubmitServlet extends HttpServlet {
 		User user = (User)session.getAttribute("user");
 		
 		List<SalesItem> salesItems = productDao.findAllSalesItems();
+		List<SalesItem> currentSalesItems = new ArrayList<>(); // 紀錄本次消費紀錄
+		
 		Map<String, String[]> map = req.getParameterMap();
 		map.entrySet().stream()
 			.filter(entry -> entry.getKey().contains("_amount"))
@@ -42,7 +46,7 @@ public class SubmitServlet extends HttpServlet {
 				String productName = entry.getKey().split("_")[0];
 				Integer productAmount = Integer.parseInt(entry.getValue()[0]);
 				if(productAmount > 0 && !productName.contains("coin")) {
-					resp.getWriter().println(productName + ":" + productAmount);
+					//resp.getWriter().println(productName + ":" + productAmount);
 					Product product = productDao.getProductByImageName(productName);
 					SalesItem salesItem = new SalesItem();
 					salesItem.setProductId(product.getId());
@@ -50,14 +54,17 @@ public class SubmitServlet extends HttpServlet {
 					salesItem.setTotalPrice(product.getPrice());
 					salesItem.setTotalAmount(productAmount);
 					salesItem.setUserId(user.getId());
-					salesItems.add(salesItem);
+					salesItems.add(salesItem); // 加入到總資料庫
+					currentSalesItems.add(salesItem); // 加入到本次消費紀錄集合
 				}
 				
 			} catch (Exception e) {
 			}
 		});
 		
-		
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/views/vending_machine/result.jsp");
+		req.setAttribute("currentSalesItems", currentSalesItems);
+		rd.forward(req, resp);
 		
 	}
 	
