da96565ea92b429233e446a526333b1fa77021ab
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-05 19:11:07 +0800

    java

--- JavaWeb20231127/src/main/java/vending_machine/dao/ProductDao.java
+++ JavaWeb20231127/src/main/java/vending_machine/dao/ProductDao.java
@@ -4,8 +4,13 @@ import java.util.List;
 
 import vending_machine.entity.Product;
 import vending_machine.entity.SalesItem;
+import vending_machine.entity.User;
 
 public interface ProductDao {
+	List<User> findAllUsers();
+	void addUser(User user);
+	User getUser(String username);
+	
 	List<Product> findAllProducts();
 	List<SalesItem> findAllSalesItems();
 	Product getProductByName(String name);
