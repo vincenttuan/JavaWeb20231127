6ddd23b6dea42d9ea54459530e839d9909c89104
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-05 20:41:48 +0800

    java

---
+++ JavaWeb20231127/src/main/java/vending_machine/servlet/LogoutServlet.java
@@ -0,0 +1,28 @@
+package vending_machine.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/vending/logout")
+public class LogoutServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		session.invalidate(); // session 失效
+		
+		// 指向 /vending/main 首頁
+		resp.sendRedirect("./main");
+		
+		
+				
+	}
+	
+}
