c9800135830396e0cc7fc7d185d26f7a7f165ade
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-08 19:24:34 +0800

    java

---
+++ JavaWeb20231127/src/main/java/vending_machine/dao/ProductDaoMySQL.java
@@ -0,0 +1,59 @@
+package vending_machine.dao;
+
+import java.util.List;
+
+import vending_machine.entity.Product;
+import vending_machine.entity.SalesItem;
+import vending_machine.entity.User;
+
+public class ProductDaoMySQL implements ProductDao {
+
+	@Override
+	public List<User> findAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public User getUser(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Product> findAllProducts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<SalesItem> findAllSalesItems() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product getProductByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product getProductByImageName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product getProductById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
