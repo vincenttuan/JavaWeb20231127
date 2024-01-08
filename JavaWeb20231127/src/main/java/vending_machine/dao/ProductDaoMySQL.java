2f00af21720de4e8db2bb41af3b3c0183f2c0601
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-08 19:39:25 +0800

    java

--- JavaWeb20231127/src/main/java/vending_machine/dao/ProductDaoMySQL.java
+++ JavaWeb20231127/src/main/java/vending_machine/dao/ProductDaoMySQL.java
@@ -2,12 +2,34 @@ package vending_machine.dao;
 
 import java.util.List;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
 import vending_machine.entity.Product;
 import vending_machine.entity.SalesItem;
 import vending_machine.entity.User;
 
 public class ProductDaoMySQL implements ProductDao {
-
+	
+private JdbcTemplate jdbcTemplate;
+	
+	public ProductDaoMySQL() {
+		String driverName = "com.mysql.cj.jdbc.Driver";
+		String dbURL = "jdbc:mysql://localhost:3306/vending_machine?serverTimezone=Asia/Taipei";
+		String username = "root";
+		String password = "12345678";
+		
+		// 設定資料來源
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(driverName);
+		dataSource.setUrl(dbURL);
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
+		
+		// 將資料來源注入給 jdbcTemplate
+		jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
 	@Override
 	public List<User> findAllUsers() {
 		// TODO Auto-generated method stub
