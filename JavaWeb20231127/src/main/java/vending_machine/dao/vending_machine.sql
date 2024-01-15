b862813240805a54557759e8414d4956570282ac
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-15 19:14:27 +0800

    java

--- JavaWeb20231127/src/main/java/vending_machine/dao/vending_machine.sql
+++ JavaWeb20231127/src/main/java/vending_machine/dao/vending_machine.sql
@@ -7,7 +7,8 @@ drop table if exists product;
 create table if not exists user(
 	id int primary key auto_increment,
     username varchar(50),
-    password varchar(50),
+    password varchar(255),
+    salt varchar(255),
     email varchar(50)
 );
 
@@ -35,10 +36,10 @@ create table if not exists sales_item(
     foreign key (user_id) references user(id)
 );
 
--- 新增 user 資料紀錄
-insert into user(username, password, email) values("john", "1234", "john@gmail.com");
-insert into user(username, password, email) values("jack", "5678", "jack@gmail.com");
-insert into user(username, password, email) values("rose", "1111", "rose@gmail.com");
+-- 新增 user 資料紀錄(利用 SecurityUtils.java 產生預設資料)
+insert into user(username, password, salt, email values('john', 'T88tUPF9a35dhABVzq88/ZVs39neEg7DLJZoulwzHc4=', 'NST/uZpn2joxRkEoyclZWA==', 'john@gmail.com')
+insert into user(username, password, salt, email values('jack', 'VEWiH/ArCIrrrbSh35I1YwVtPEPch7gMsuGsslFaCiI=', 'yBGAibcA1+5NtJIIsmZfxg==', 'jack@gmail.com')
+insert into user(username, password, salt, email values('rose', 'hMmIFnQ5Y75nTR3WZvyIVsJAyJ0onbYpa613b11CRLk=', 'k0c1MX8w4itp0ooNrxHYjg==', 'rose@gmail.com')
 
 -- 新增 product 資料紀錄
 insert into product(name, image_name, price, stock) values("台灣啤酒", "beer", 50, 5);
