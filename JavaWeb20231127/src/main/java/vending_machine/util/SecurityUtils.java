f3f22a11234ef177dedff754f211a1c79ef83dcd
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-15 19:00:36 +0800

    java

--- JavaWeb20231127/src/main/java/vending_machine/util/SecurityUtils.java
+++ JavaWeb20231127/src/main/java/vending_machine/util/SecurityUtils.java
@@ -1,7 +1,9 @@
 package vending_machine.util;
 
 import java.math.BigInteger;
+import java.security.MessageDigest;
 import java.security.SecureRandom;
+import java.util.Base64;
 
 /**
  * 用於生成鹽(salt)和哈希(Hash)密碼的工具
@@ -13,8 +15,26 @@ public class SecurityUtils {
 		SecureRandom random = new SecureRandom();
 		byte[] salt = new byte[16];
 		random.nextBytes(salt); // 隨機生成一組 byte[] 資訊
-		String saltString = new BigInteger(1, salt).toString(); // 將 byte[] 轉換成一個數字字串, 1 表示正數
-		return saltString;
+		// 將鹽轉大正整數
+		// return new BigInteger(1, salt).toString(); // 將 byte[] 轉換成一個數字字串, 1 表示正數
+		// 將鹽轉base64(比較常見)
+		return Base64.getEncoder().encodeToString(salt);
+	}
+	
+	// 生成哈希
+	public static String hashPassword(String password, String salt) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			md.update((salt + password).getBytes()); // 鹽 + password(明碼)
+			byte[] hashedPassword = md.digest(); // 取得哈希值
+			// 將哈希值轉一個大正整數
+			//return new BigInteger(1, hashedPassword).toString();
+			// 將哈希轉base64(比較常見)
+			return Base64.getEncoder().encodeToString(hashedPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 	
 	public static void main(String[] args) {
