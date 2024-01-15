65b2a5124fc3d6429523c9a2dac9137d6b84317e
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-15 19:35:36 +0800

    java

--- JavaWeb20231127/src/main/java/vending_machine/util/Test.java
+++ JavaWeb20231127/src/main/java/vending_machine/util/Test.java
@@ -8,8 +8,9 @@ public class Test {
 		2	jack	f4GwKKP/uMoYNkS3zO38rdqTF8psWW+ITIUcy+R/LaY=	pIDHnorSH7B/VcPdSVmZyQ==	jack@gmail.com
 		3	rose	LKaCzacdQIni+M+99r2EgqiD7TLiPCvacrm2dGhMh7U=	injLTEco9KdKXgfD7QwncQ==	rose@gmail.com
 		*/
-		
-		
+		// 猜 john 的密碼
+		System.out.println("密碼 1111: hash: " + SecurityUtils.getHashPassword("1111", "dkBTXgJMWWdcPcOwC2/icg==") );
+		System.out.println("密碼 1234: hash: " + SecurityUtils.getHashPassword("1234", "dkBTXgJMWWdcPcOwC2/icg==") );
 	}
 
 }
