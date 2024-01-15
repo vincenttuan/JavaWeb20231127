75e909d55b53bb3ef12c180a1cb6717ea900b707
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-15 21:11:47 +0800

    java

--- JavaWeb20231127/src/main/java/vending_machine/entity/SalesItem.java
+++ JavaWeb20231127/src/main/java/vending_machine/entity/SalesItem.java
@@ -1,6 +1,13 @@
 package vending_machine.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 // 銷貨紀錄
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SalesItem {
 	private Integer id; // 銷貨 id
 	private Integer productId; // 商品 id
@@ -9,66 +16,6 @@ public class SalesItem {
 	private Integer totalAmount; // 銷售數量
 	// 該銷售紀錄是屬於哪一個使用者 
 	private Integer userId; // 使用者 id
-	
-	public SalesItem() {
-		
-	}
-	
-	public SalesItem(Integer id, Integer productId, String productName, Integer totalPrice, Integer totalAmount, Integer userId) {
-		this.id = id;
-		this.productId = productId;
-		this.productName = productName;
-		this.totalPrice = totalPrice;
-		this.totalAmount = totalAmount;
-		this.userId = userId;
-	}
-	
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public Integer getProductId() {
-		return productId;
-	}
-	public void setProductId(Integer productId) {
-		this.productId = productId;
-	}
-	public String getProductName() {
-		return productName;
-	}
-	public void setProductName(String productName) {
-		this.productName = productName;
-	}
-	public Integer getTotalPrice() {
-		return totalPrice;
-	}
-	public void setTotalPrice(Integer totalPrice) {
-		this.totalPrice = totalPrice;
-	}
-	public Integer getTotalAmount() {
-		return totalAmount;
-	}
-	public void setTotalAmount(Integer totalAmount) {
-		this.totalAmount = totalAmount;
-	}
-
-	public Integer getUserId() {
-		return userId;
-	}
-
-	public void setUserId(Integer userId) {
-		this.userId = userId;
-	}
-
-	@Override
-	public String toString() {
-		return "SalesItem [id=" + id + ", productId=" + productId + ", productName=" + productName + ", totalPrice="
-				+ totalPrice + ", totalAmount=" + totalAmount + ", userId=" + userId + "]";
-	}
 	
 	
 }
--- JavaWeb20231127/src/main/java/vending_machine/entity/User.java
+++ JavaWeb20231127/src/main/java/vending_machine/entity/User.java
@@ -1,70 +1,18 @@
 package vending_machine.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 	private Integer id;
 	private String username;
 	private String password;
 	private String salt;
 	private String email;
-	
-	public User() {
-		
-	}
-	
-	public User(Integer id, String username, String password, String salt, String email) {
-		this.id = id;
-		this.username = username;
-		this.password = password;
-		this.salt = salt;
-		this.email = email;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getSalt() {
-		return salt;
-	}
-
-	public void setSalt(String salt) {
-		this.salt = salt;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", username=" + username + ", password=" + password + ", salt=" + salt + ", email="
-				+ email + "]";
-	}
-
 	
 	
 	
