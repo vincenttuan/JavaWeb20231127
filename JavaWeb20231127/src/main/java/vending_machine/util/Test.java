4004f3c39b15fd75ab04d3eb48d54c328b3739fe
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-15 19:26:29 +0800

    java

---
+++ JavaWeb20231127/src/main/java/vending_machine/util/Test.java
@@ -0,0 +1,15 @@
+package vending_machine.util;
+
+public class Test {
+
+	public static void main(String[] args) {
+		/*
+		1	john	CvYZAzr78PYhyTxDs2CJ91hhauo3+KUITuoK9ewWQ90=	dkBTXgJMWWdcPcOwC2/icg==	john@gmail.com
+		2	jack	f4GwKKP/uMoYNkS3zO38rdqTF8psWW+ITIUcy+R/LaY=	pIDHnorSH7B/VcPdSVmZyQ==	jack@gmail.com
+		3	rose	LKaCzacdQIni+M+99r2EgqiD7TLiPCvacrm2dGhMh7U=	injLTEco9KdKXgfD7QwncQ==	rose@gmail.com
+		*/
+		
+		
+	}
+
+}
