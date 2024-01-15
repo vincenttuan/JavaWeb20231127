e965bb2eb318323f3da644d97525e1c325e54e86
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-15 21:11:00 +0800

    java

--- JavaWeb20231127/src/main/java/vending_machine/entity/Product.java
+++ JavaWeb20231127/src/main/java/vending_machine/entity/Product.java
@@ -1,5 +1,14 @@
 package vending_machine.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
 	private Integer id;
 	private String name;
@@ -7,47 +16,6 @@ public class Product {
 	private Integer price;
 	private Integer stock;
 	
-	public Product() {
-		
-	}
-	
-	public Product(Integer id, String name, String imageName, Integer price, Integer stock) {
-		this.id = id;
-		this.name = name;
-		this.imageName = imageName;
-		this.price = price;
-		this.stock = stock;
-	}
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getImageName() {
-		return imageName;
-	}
-	public void setImageName(String imageName) {
-		this.imageName = imageName;
-	}
-	public Integer getPrice() {
-		return price;
-	}
-	public void setPrice(Integer price) {
-		this.price = price;
-	}
-	public Integer getStock() {
-		return stock;
-	}
-	public void setStock(Integer stock) {
-		this.stock = stock;
-	}
 	
 	
 }
